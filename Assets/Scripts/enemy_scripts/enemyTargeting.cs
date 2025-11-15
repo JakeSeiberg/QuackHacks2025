6b4ed1c7f36b21bb79f6b50ac80a86b8a7df6263
Ben Elster <belster1024@gmail.com>
2025-11-15 12:40:42 -0800

    created new script folder, testing

--- Assets/Scripts/enemy_scripts/enemyTargeting.cs
+++ Assets/Scripts/enemy_scripts/enemyTargeting.cs
@@ -16,16 +16,7 @@ public class enemyTargeting : MonoBehaviour
         Vector3 direction = player.transform.position - transform.position;
         rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * bulletspeed;
         float rot = Mathf.Atan2(-direction.y,-direction.x) * Mathf.Rad2Deg;
-        transform.rotation= Quaternion.Euler(0,0,rot+90);
-        Destroy(GameObject, lifetime);        
-    }
-    void OnTriggerEnter2D(Collider2D other){
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Hit Player!");
-        }
-
-        Destroy(gameObject);
+        transform.rotation= Quaternion.Euler(0,0,rot+90);      
     }
         
 }
